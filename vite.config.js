d8a468bdcc6608d40eaa3fcd3d429468926932ca
Ashish Singh <ashish.ksingh8765@gmail.com>
2024-04-10 01:46:33 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "jsm-jj",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "dexteroin",
+    project: "iphone"
+  }), sentryVitePlugin({
+    org: "jsm-hb0",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
+})
